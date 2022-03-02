14341b6262881548ac8a3ef1fe9d9006fb912f6d
Anupam Arya <anu9am@gmail.com>
2022-03-03 04:23:39 +0530

    saving user details with encrypted password

--- server/app/Routes/jwtAuth.js
+++ server/app/Routes/jwtAuth.js
@@ -1,5 +1,6 @@
 const pool = require('./../../config/db.config');
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 
 router.post("/register", async (req, res) => {
 
@@ -12,17 +13,21 @@ router.post("/register", async (req, res) => {
         // console.log(typeof user); returns object
         // console.log(user.rows);
         if (user.rows.length !== 0) {
-            res.status(400).json({
-                status: "error",
-                message: "User already exists"
-            });
+            res.status(400).json("User already exists");
         }
 
         // bcrypt the user password
-        const saltRound = 10;
+        const saltRound = 10; // Try randomising the salt rounds
         const salt = await bcrypt.genSalt(saltRound);
+        const bcryptPassword = await bcrypt.hash(user_password, salt); // encrypted password
 
-        const bcryptPassword = await bcrypt.hash(user_password, salt);
+        // insert user into database
+        const newUser = await pool.query("Insert INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *", [user_name, user_email, bcryptPassword]);
+
+        res.json(newUser.rows[0]);
+
+        // generate jwt token
+
 
     } catch (error) {
         console.error(error.message);
