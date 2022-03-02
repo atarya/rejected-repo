c265f1d5c34bbd1227f5d25db16e4259f207e559
Anupam Arya <anu9am@gmail.com>
2022-03-03 04:49:41 +0530

    added jwt Genereator function

---
+++ server/app/Utils/jwtGenerator.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+jwtGenerator = () => {
+
+    // choosing reference for the token
+    const payload = {
+        user: user_id
+    }
+    // generating token using the secret key and setting expiry to 1 hour
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 60 * 60 });
+    return token;
+}
+
+module.exports = jwtGenerator;
